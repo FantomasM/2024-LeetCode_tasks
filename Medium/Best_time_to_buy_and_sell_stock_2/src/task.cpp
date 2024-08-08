58ad748f8406b1ce29ceb2f7e9d8a033ad035540
Fantomas Michael <fantomasm@Debian-FantomasM.FantomasM.com>
2024-08-08 12:44:26 +0200

    Update solution to best time to buy and sell 2

--- Medium/Best_time_to_buy_and_sell_stock_2/src/task.cpp
+++ Medium/Best_time_to_buy_and_sell_stock_2/src/task.cpp
@@ -7,42 +7,23 @@ using namespace std;
 
 class Solution {
 public:
-    void recursive_profit(vector<int> & prices,int cur_profit, bool has_stock,int stock_value,int cur_index,int & best_value) {
-
-        if(cur_index>=(int)prices.size()-1) {
-            if(has_stock) {
-                if(cur_profit-stock_value+prices[cur_index]>best_value) {
-                    best_value=cur_profit-stock_value+prices[cur_index];
-                }
-
-            }
-            else {
-                if(cur_profit>best_value) {
-                    best_value=cur_profit;
-                }
-
-            }
-            return;
-        }
-        for(size_t i=(size_t)cur_index+1;i<prices.size();i++) {
-            if(has_stock) {
-                recursive_profit(prices,cur_profit-stock_value+prices[i],false,0,(int)i,best_value);
-
-            }
-            else {
-                recursive_profit(prices,cur_profit,true,prices[i],(int)i,best_value);
-
-            }
-        }
-
-    }
+   
 
     int maxProfit(vector<int>& prices) {
-        int profit=0;
-        int best_profit=0;
-        recursive_profit(prices,profit,false,0,-1,best_profit);
 
-        return best_profit;
+
+        int profit=0;
+        int start=prices[0];
+        for(size_t i=1;i<prices.size();i++) {
+            if(start<prices[i]) {
+                profit+=prices[i]-start;
+            }
+            start=prices[i];
+        }
+        return profit;
+
+
+
     }
 };
 
