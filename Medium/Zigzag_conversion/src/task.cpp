1015630dfbf0d78f56f28f7b41a473956f0ab929
Fantomas Michael <fantomasm@Debian-FantomasM.FantomasM.com>
2024-08-21 13:13:17 +0200

    Uploading task solution to Zigzag conversion task

--- Medium/Zigzag_conversion/src/task.cpp
+++ Medium/Zigzag_conversion/src/task.cpp
@@ -3,25 +3,53 @@
 #include <vector>
 #include <string>
 #include <cassert>
-#include <ctype.h>
+
 using namespace std;
+
 class Solution {
 public:
-    string convert(string s, int numRows) {
-        string tmp;
-        for(auto el : s) {
-            if(isalpha(el) ||el=='.' ||el==',') {
-                tmp.push_back(el);
-            }
+    int row_distribution(int index, int num_rows) {
+        int possibilities = num_rows+num_rows-2;
+        int new_index=index%possibilities;
+        if(new_index<num_rows) {
+            return new_index;
         }
-        std::cout <<tmp <<std::endl;
-        return tmp;
+        else {
+            return (new_index-possibilities)*-1;
+        }
+    }
+    string convert(const string & s, int numRows) {
+        if(numRows==1) {
+            return s;
+        }
+        std::vector<string> tmp(numRows);
+        for(int i = 0; i < (int)s.size(); i++) {
+            tmp[row_distribution(i, numRows)].push_back(s[i]);
+        }
+        string result = "";
+        for(int i = 0; i < (int)tmp.size(); i++) {
+            result += tmp[i];
+        }
+        return result;
+
     }
 };
 
 int main(void) {
     Solution sol;
     string first="PAYPALISHIRING";
+    assert(sol.row_distribution(0,4)==0);
+    assert(sol.row_distribution(1,4)==1);
+    assert(sol.row_distribution(2,4)==2);
+    assert(sol.row_distribution(3,4)==3);
+    assert(sol.row_distribution(4,4)==2);
+    assert(sol.row_distribution(5,4)==1);
+
+    assert(sol.row_distribution(0,3)==0);
+    assert(sol.row_distribution(1,3)==1);
+    assert(sol.row_distribution(2,3)==2);
+    assert(sol.row_distribution(3,3)==1);
+
     assert(sol.convert(first,3)=="PAHNAPLSIIGYIR");
 
     string second="PAYPALISHIRING";
